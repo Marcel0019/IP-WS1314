ca405bb5bf83dd3b46a6505e0b8069b7f0725490
Marcel Wallbaum <marcel.wallbaum@gmail.com>
2013-11-11 20:21:39 +0100

    ESA2

---
+++ esa2/php/kontakt/kontakt.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+  * Internet Programmierung
+  *
+  *
+  * @package	Modul Internet-Programmierung
+  * @author	Marcel Wallbaum <marcel.wallbaum@gmail.com>
+  * @copyright	2013 Marcel Wallbaum
+  *
+  */
+
+$nachname = isset($_POST["nachname"]) ? $_POST["nachname"] : "";
+echo "Der Nachmane lautet $nachname";
+?>
