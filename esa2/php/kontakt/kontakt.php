b62727a7ecf4e9a5cdb3b91e844e9b14afc4ddeb
Marcel Wallbaum <marcel.wallbaum@gmail.com>
2013-11-14 19:58:38 +0100

    change

--- esa2/php/kontakt/kontakt.php
+++ esa2/php/kontakt/kontakt.php
@@ -10,6 +10,33 @@
   *
   */
 
-$nachname = isset($_POST["nachname"]) ? $_POST["nachname"] : "";
-echo "Der Nachmane lautet $nachname";
+//Prüfen ob der Vorname vorhanden ist und nicht leer
+//mittels htmlentities eine codierte Zeichenkette zurückgeben
+if(!isset($_POST["vorname"] Or Trim($_POST['vorname']) == "")){
+    $fehler = 1;
+    $fehlertext = "Bitte geben Sie einen gültigen Vornamen ein.<br>";
+} else {
+    $vorname = htmlentities($_POST["vorname"]);
+}
+
+//Prüfen ob der Nachname vorhanden ist und nicht leer
+//mittels htmlentities eine codierte Zeichenkette zurückgeben
+if(!isset($_POST["nachname"] Or Trim($_POST['nachname']) == "")){
+    $fehler = 1;
+    $fehlertext = "Bitte geben Sie einen gültigen Nachname ein.<br>";
+} else {
+    $vorname = htmlentities($_POST["nachname"]);
+}
+
+//Prüfen ob der Nachname vorhanden ist und nicht leer
+//mittels htmlentities eine codierte Zeichenkette zurückgeben
+if(!isset($_POST["matrikelnr"]) Or is_numeric($_POST["matrikelnr"])){
+    $fehler = 1;
+    $fehlertext = "Bitte geben Sie eine gültige Matrikelnummer ein.<br>";
+} else {
+    $vorname = htmlentities($_POST["nachname"]);
+}
+
+
+
 ?>
